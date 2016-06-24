bbf42fc2676fa8d5515661e905bc505cc65518c5
Taylor Welling <tw3080@gmail.com>
2016-06-23 20:15:26 -0700

    added getLocationId function to turn lat/lng into isntagram location-ids

--- js/app.js
+++ js/app.js
@@ -1,14 +1,23 @@
+/*
+Instagram access token: 12026246.e899233.1086d94709f141a1b483c6c135202080
+*/
+
 // Initialize map
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    // Center initializes to Seattle, WA
+    // TODO: possibly use user's geolocation to initially center map?
+    center: {lat: 47.60621, lng: -122.332071
+},
     zoom: 12
   });
   var geocoder = new google.maps.Geocoder();
   // Submit button click event
   // TODO: pressing enter should have same affect as clicking button
-  document.getElementById('submit').addEventListener('click', function() {
+  $('.search-bar').submit(function(e) {
+    e.preventDefault();
     geocodeAddress(geocoder, map);
+    // var location = $(this).find('#address').val();
   });
 }
 
@@ -16,6 +25,14 @@ function initMap() {
 function geocodeAddress(geocoder, resultsMap) {
   var address = document.getElementById('address').value;
   geocoder.geocode({'address': address}, function(results, status) {
+    // Global latititude and longitude variables
+    lat = results[0].geometry.location.lat();
+    lng = results[0].geometry.location.lng();
+    console.log(lat);
+    console.log(lng);
+    getLocationId(lat, lng);
+    // console.log(lat);
+    // console.log(lng);
     if (status === google.maps.GeocoderStatus.OK) {
       resultsMap.setCenter(results[0].geometry.location);
       // Adds marker for each searched address on map
@@ -30,3 +47,41 @@ function geocodeAddress(geocoder, resultsMap) {
     }
   });
 }
+
+/* Convert lat/long returned by geocodeAddress() to a location-id for use with
+   instagram API */
+function getLocationId(lat, long) {
+  var locationId = 'lat=' + lat + '&lng=' + lng;
+  var accessToken = '12026246.e899233.1086d94709f141a1b483c6c135202080';
+  var url = 'https://api.instagram.com/v1/locations/search?' + locationId + '&access_token=' + accessToken;
+  $.ajax({
+    url: url,
+    dataType: "jsonp",
+    type: "GET",
+    distance: 0,
+    success: function(response) {
+      console.log(response);
+    }
+  });
+}
+
+// Gets Instagram posts by location
+function getPosts(location) {
+  var locationId = location;
+  var accessToken = '12026246.e899233.1086d94709f141a1b483c6c135202080';
+  var url = 'https://api.instagram.com/v1/locations/' + locationId + '/media/recent?access_token=' + accessToken;
+  $.ajax({
+    url: url,
+    dataType: "jsonp",
+    type: "GET",
+    success: function(response) {
+      console.log(response);
+    }
+  });
+}
+
+$(function() {
+  $('.search-bar').submit(function(e) {
+
+  });
+});
