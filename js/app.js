4b4c7d12f49076fb7f11f112308732fc04b920c9
Taylor Welling <tw3080@gmail.com>
2016-06-27 18:14:42 -0700

    added custom styles for map

--- js/app.js
+++ js/app.js
@@ -14,6 +14,205 @@ var page = 1; // Flickr page number
 
 // Initialize map
 function initMap() {
+  // Array of custom map styles
+  var styles = [
+    {
+      "featureType": "water",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#d3d3d3"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "stylers": [
+        {
+          "color": "#808080"
+        },
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "visibility": "on"
+        },
+        {
+          "color": "#b3b3b3"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#ffffff"
+        }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "visibility": "on"
+        },
+        {
+          "color": "#ffffff"
+        },
+        {
+          "weight": 1.8
+        }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#d7d7d7"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "visibility": "on"
+        },
+        {
+          "color": "#ebebeb"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#a7a7a7"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#ffffff"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#ffffff"
+        }
+      ]
+    },
+    {
+      "featureType": "landscape",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "visibility": "on"
+        },
+        {
+          "color": "#efefef"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#696969"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "visibility": "on"
+        },
+        {
+          "color": "#737373"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.locality",
+      "stylers": [
+        {
+          "weight": 0.1
+        },
+        {
+          "color": "#000000"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#d6d6d6"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {},
+    {
+      "featureType": "poi",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#dadada"
+        }
+      ]
+    }
+];
+
+  // Map object
   var mapProp = {
     // Center initializes to Seattle, WA
     /* TODO: possibly use user's geolocation to initially center map? Or just
@@ -22,6 +221,10 @@ function initMap() {
     zoom: 15 // Zoom defaults to city-level
   };
   map = new google.maps.Map(document.getElementById('map'), mapProp);
+
+  // Sets map styles
+  map.setOptions({styles: styles});
+
   // Re-centers map on window resize
   google.maps.event.addDomListener(window, 'resize', function() {
     var center = map.getCenter();
@@ -83,6 +286,7 @@ function getPhotos(coordinate) {
     // console.log(response);
     var infoWindow = new google.maps.InfoWindow();
     $.each(response.photos.photo, function(i, item) {
+      console.log(item);
       var url = 'http://farm' +
         item.farm +
         '.staticflickr.com/' +
@@ -115,7 +319,7 @@ function getPhotos(coordinate) {
          displaying the details of a picture */
       google.maps.event.addListener(marker, 'click', function() {
         // TODO: How do I add jQuery to setContent?
-        infoWindow.setContent('<img src="' + url + '"/>');
+        infoWindow.setContent('<img class="infoThumb" src="' + url + '"/>');
         infoWindow.open(map, marker);
       });
       $(thumbnail).click(function() {
@@ -129,7 +333,7 @@ function getPhotos(coordinate) {
       */
     });
     // Arranges images in a justified Gallery
-    gallery.justifiedGallery();
+    // gallery.justifiedGallery();
   })
   // TODO: Add a more descriptive error alert/message
   .fail(function(jqXHR, error) {
